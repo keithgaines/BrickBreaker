d9ed951665eadf6db34e432413f11f9291010426
Keith Gaines <patrick.keith.gaines@gmail.com>
2022-12-18 13:17:57 -0600

    setup window

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ from scoreboard import Scoreboard
 
 # sets up screen
 screen = Screen()
-screen.setup(width=800, height=600)
+screen.setup(width=600, height=800)
 screen.bgcolor("black")
 screen.title("Brick Breaker")
 screen.tracer(0)
+
+screen.exitonclick()
