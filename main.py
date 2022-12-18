5cf6eb55ae6280e9651ceed671deed8ef0d230b8
Keith Gaines <patrick.keith.gaines@gmail.com>
2022-12-18 13:15:13 -0600

    setup screen

---
+++ main.py
@@ -0,0 +1,12 @@
+import time
+from turtle import Screen
+
+from paddle import Paddle
+from scoreboard import Scoreboard
+
+# sets up screen
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Brick Breaker")
+screen.tracer(0)
