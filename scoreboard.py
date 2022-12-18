591319e79e6df3f9e155b65c2135c906683173a6
Keith Gaines <patrick.keith.gaines@gmail.com>
2022-12-18 14:02:39 -0600

    removed 2nd player board

--- scoreboard.py
+++ scoreboard.py
@@ -16,14 +16,8 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.clear()
-        self.goto(-100, 200)
-        self.write(arg=f"{self.l_score}", align=ALIGNMENT, font=FONT)
         self.goto(100, 200)
         self.write(arg=f"{self.r_score}", align=ALIGNMENT, font=FONT)
-
-    def l_point(self):
-        self.l_score += 1
-        self.update_scoreboard()
 
     def r_point(self):
         self.r_score += 1
