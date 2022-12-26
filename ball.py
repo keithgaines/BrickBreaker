c6328a5d54a356c1f2772034d5d2e637410c7ac6
Keith Gaines <patrick.keith.gaines@gmail.com>
2022-12-26 14:09:45 -0600

    moved bounce functions into main

--- ball.py
+++ ball.py
@@ -1,31 +1,16 @@
 from turtle import Turtle
-
+import random
 
 class Ball(Turtle):
     def __init__(self):
         super().__init__()
-        self.color("white")
         self.shape("circle")
+        self.color("white")
         self.penup()
+        self.goto(-50,-210)
+        self.setheading(random.randint(1,179))
         self.x_move = 10
         self.y_move = 10
         self.move_speed = 0.1
 
-    def move(self):
-        new_x = self.xcor() + self.x_move
-        new_y = self.ycor() + self.y_move
-        self.goto(new_x, new_y)
 
-    # makes ball move in opposite direction once it hits either the top of bottom wall
-    def bounce_y(self):
-        self.y_move *= -1
-
-    # makes ball move in opposite direction once it has collided with a paddle
-    def bounce_x(self):
-        self.x_move *= -1
-        self.move_speed *= 0.7
-
-    def reset_position(self):
-        self.goto(0, 0)
-        self.move_speed = 0.1
-        self.bounce_x()
